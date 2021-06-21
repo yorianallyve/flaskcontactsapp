125ff4215c6e96e598c7a856608dc9c58773883b
YoriaDigitalCG <yrios@digitalcg.com.co>
2021-06-21 13:23:44 -0500

    .

--- App.py
+++ App.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
 
 app=Flask(__name__)
@@ -13,11 +13,16 @@ mysql = MySQL(app)
 
 @app.route('/')
 def index():
-    return 'test'
+    return render_template('index.html')
 
-@app.route('/add_contact')
+@app.route('/add_contact', methods=['POST'])
 def add_contact():
-    return 'add contact'
+      if request.method == 'POST':
+        fullname = request.form['fullname']
+        phone = request.form['phone']
+        email = request.form['email']
+        print(fullname, phone, email)
+        return 'recibido'
 
 @app.route('/edit')
 def edit_contact():
